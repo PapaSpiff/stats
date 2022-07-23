f2c4c315cbf2e9d656a969f3c18063765cd232e5
PapaSpiff <papaspiff.spl@gmail.com>
2022-07-23 14:09:33 +0200

    beginning to split things into more manageable chunks

---
+++ special.py
@@ -0,0 +1,73 @@
+class Special:
+    special_ids = {
+        2: 'pitcher',
+        7: 'presser',
+        8: 'jetpack', 
+        9: 'chakuchi'
+    }
+    special_en = {
+        'presser' : "Sting Ray",
+        'jetpack' : "Inkjet",
+        'chakuchi': "Splashdown",
+        'pitcher' : "Bomb Launcher"
+    }
+    special_fr = {
+        'presser' : "Pigmalance",
+        'jetpack' : "Chromo-jet",
+        'chakuchi': "Choc chromatique",
+        'pitcher' : "Lance-bombes splash"
+    }
+    special_ja = {
+        'presser' : "ハイパープレッサー",
+        'jetpack' : "ジェットパック",
+        'chakuchi': "スーパーチャクチ",
+        'pitcher' : "ボムピッチャ"
+    }
+    special_str = special_en
+
+    special_img = {
+        'presser' : "255dbac123ae410fb6418a3f9fd8fa4b.png",
+        'jetpack' : "55b49f3fba5f914469c9fff532db4fd2.png",
+        'chakuchi': "aa9dbd404a5d30229a9576c8930adc9c.png",
+        'pitcher' : "5e78522afbc7115cf77b337e910c8798.png"
+    }
+
+    title = {
+        'en' : 'Special weapons',
+        'fr' : 'Armes spéciales',
+        'ja' : 'スペシャルウェポン'
+    }
+
+    def to_img(key):        
+        return Special.special_img[key]
+
+    def title_str(lang):
+        if lang in Special.title:
+            return Special.title[lang]
+        return Special.title['en']
+
+    def set_language(self, lang):
+        if lang == "en":
+            self.special_str = Special.special_en
+        elif lang == "fr":
+            self.special_str = Special.special_fr
+        elif lang == "ja":
+            self.special_str = Special.special_ja
+        else: #current default to en
+            self.special_str = Special.special_en
+
+    def __str__(self):
+        return self.special_str[self.key]
+
+    def to_str(key, lang):
+        if lang == "en":
+            return Special.special_en[key]
+        elif lang == "fr":
+            return Special.special_fr[key]
+        elif lang == "ja":
+            return Special.special_ja[key]
+        else: #current default to en
+            return Special.special_en[key]
+
+    def __init__(self, id):
+        self.key = self.special_ids[int(id)]
---
+++ wave.py
@@ -0,0 +1,118 @@
+class Wave:
+    # https://splatoonwiki.org/wiki/Salmon_Run_data#Wave_type
+    wave_percent_max = 2
+    night_wave_percent_max = 0.7
+    day_avg      = 75
+    night_avg    = 25
+    low_avg      = 20
+    normal_avg   = 60
+    high_avg     = 20
+    ms_avg       = 4.2
+    ms_ht_avg    = 0.8
+    ms_nt_avg    = 2.5
+    ms_lt_avg    = 0.8
+    seek_avg     = 4.2
+    seek_ht_avg  = 1.0
+    seek_nt_avg  = 3.1
+    cohock_avg   = 4.2
+    rush_avg     = 4.2
+    rush_ht_avg  = 1.0
+    rush_nt_avg  = 3.1
+    grill_avg    = 4.2
+    grill_ht_avg = 1.0
+    grill_nt_avg = 3.1
+    fog_avg      = 4.2
+    fog_ht_avg   = 0.8
+    fog_nt_avg   = 2.5
+    fog_lt_avg   = 0.8
+
+    waves_en = {
+        'day' : "Day",
+        'days' : "Days",
+        'night' : "Night",
+        'nights' : "Nights",
+        'tide' : "Water levels",
+        'high' : "High tide",
+        'low' : "Low tide",
+        'normal' : "Normal",
+        'rush' : "Rush",
+        'fog' : "Fog",
+        'griller' : "The Griller",
+        'cohock-charge' : "Cohock Charge",
+        'goldie-seeking' : "Goldie Seeking",
+        'the-mothership' : "The Mothership",
+        'water-levels' : "-"
+
+    }
+    waves_fr = {
+        'day' : "Jour",
+        'days': "Jours",
+        'night' : "Nuit",
+        'nights' : "Nuits",
+        'tide' : "Niveau de Marée",
+        'high' : "Marée haute",
+        'low' : "Marée basse",
+        'normal' : "Mi-marée",
+        'rush' : "Ruées",
+        'fog' : "Brouillard",
+        'griller' : "Barbeurks",
+        'cohock-charge' : "Charges de Sumoches",
+        'goldie-seeking' : "Chasse aux Dorax",
+        'the-mothership' : "Vaisseaux Mères",
+        'water-levels' : "-"
+    }
+
+    waves_ja = {
+        'day' : "昼間", #FIXME
+        'days' : "昼間", #FIXME
+        'night' : "夜", #FIXME
+        'nights' : "夜", #FIXME
+        'tide' : "水位",
+        'high' : "満潮",
+        'low' : "干潮",
+        'normal' : "通常", # FIXME?
+        'rush' : "ラッシュ",
+        'fog' : "霧",
+        'griller' : "グリル発進",
+        'cohock-charge' : "ドスコイ大量発生",
+        'goldie-seeking' : "キンシャケ探し",
+        'the-mothership' : "ハコビヤ襲来",
+        'water-levels' : "-"
+    }
+
+    other_en = {
+        'title' : "Waves",
+        'titlemax' : "Team total Golden Eggs",
+        'average'  : "Average",
+    }
+    other_fr = {
+        'title' : "Vagues",
+        'titlemax' : "Totaux d'œufs dorés d'équipe",
+        'average'  : "Moyenne",
+    }
+    other_ja = {
+        'title' : "WAVE",
+        'titlemax' : "チーム合計の金イクラ納品数",
+        'average'  : "平均",
+    }
+
+    def other_str(key, lang):
+        if lang == "en":
+            return Wave.other_en[key]
+        elif lang == "fr":
+            return Wave.other_fr[key]
+        elif lang == "ja":
+            return Wave.other_ja[key]
+        else: #current default to en
+            return Wave.other_en[key]
+
+    def to_str(key, lang):
+        if lang == "en":
+            return Wave.waves_en[key]
+        elif lang == "fr":
+            return Wave.waves_fr[key]
+        elif lang == "ja":
+            return Wave.waves_ja[key]
+        else: #current default to en
+            return Wave.waves_en[key]
+
