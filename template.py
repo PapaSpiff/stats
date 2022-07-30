60fde9678280cbbc7bd68686f160813a8349c10d
PapaSpiff <papaspiff.spl@gmail.com>
2022-07-31 00:25:01 +0200

    added first table of the wave card

--- template.py
+++ template.py
@@ -1,9 +1,11 @@
+from statistics import mean, pstdev
 import jinja2
 from player import Player
 from rotation import Rotation
 from gamesession import GameSession
 from weapon import Weapon
 from special import Special
+from wave import Wave
 from jinja2 import Environment, FileSystemLoader
 from typing import Union
 from time import strftime, gmtime
@@ -23,16 +25,26 @@ def weapon_name(key: str, lang: str="en") -> str:
 def special_name(key: str, lang: str="en") -> str:
     return Special.to_str(key, lang)
 
+def wave_name(key: str, lang: str="en") -> str:
+    return Wave.to_str(key, lang)
+
+def wave_other(key: str, lang: str="en") -> str:
+    return Wave.other_str(key, lang)
+
 def html_player_rotation(player: Player, rotation: Rotation, session: GameSession, lang: str="en") -> str:
     env = Environment(loader=FileSystemLoader("tpl/", encoding='utf-8'))
-    env.filters["epochgmttime"] =  epoch_to_gmt_str
-    env.filters["weapontoimg"]  =  weapon_key_to_img
-    env.filters["weaponname"]  =  weapon_name
-    env.filters["specialtoimg"]  =  special_key_to_img
-    env.filters["specialname"]  =  special_name
-
+    env.filters["epochgmttime"] = epoch_to_gmt_str
+    env.filters["weapontoimg"]  = weapon_key_to_img
+    env.filters["weaponname"]   = weapon_name
+    env.filters["specialtoimg"] = special_key_to_img
+    env.filters["specialname"]  = special_name
+    env.filters["wavename"]     = wave_name
+    env.filters["waveother"]    = wave_other
+    env.filters["mean"]         = mean
+    env.filters["pstdev"]       = pstdev
     tpl = env.get_template("player_rotation.html")
     rotation.set_language(lang)
     return tpl.render(player=player, rotation=rotation, session=session, 
                     specialtitle=Special.title_str(lang), weapontitle=Weapon.title_str(lang),
+                    waves=Wave(),
                     lang=lang)
